152e46240b8c61674cece3a9ee4073bfaed652c8
Ronald Toussaint <ronald@dekolos.com>
2018-02-02 15:28:27 +0100

    Add jasmine-console-reporter
    Added jasmine-console-reporter for nicer console output when running tests.

--- tests/index.js
+++ tests/index.js
@@ -11,4 +11,17 @@ jasmine.loadConfig({
 	stopSpecOnExpectationFailure: false,
 });
 
+// setup console reporter
+const JasmineConsoleReporter = require('jasmine-console-reporter');
+const reporter = new JasmineConsoleReporter({
+    colors: 1,           // (0|false)|(1|true)|2
+    cleanStack: 1,       // (0|false)|(1|true)|2|3
+    verbosity: 4,        // (0|false)|1|2|(3|true)|4
+    listStyle: 'indent', // "flat"|"indent"
+    activity: false
+});
+jasmine.env.clearReporters();
+jasmine.addReporter(reporter);
+
+// Execute the tests
 jasmine.execute();
