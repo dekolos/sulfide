8037364d9a3d47837070df95d2cc2e9169bc8849
Ronald Toussaint <ronald@dekolos.com>
2018-02-06 23:32:14 +0100

    Add child selector find function
    This commit adds the find method to SulfideElement. It can be
    used to find children of an element. The find method takes a
    string (css selector or xpath) or a SulfideElement as parameter.
    (The SulfideElement should be generated by a selector function)
    The find method is chainable, e.g.:
    
    $('body').find('.header').find('h1').should(exist());

---
+++ tests/specs/selectors/testChildSelectors.js
@@ -0,0 +1,41 @@
+describe('Child selectors', () => {
+	it('finds a child with a css selector', async () => {
+		await $.open('https://dekolos.github.io/sulfide/tests/todo/todo.html');
+		await $('body').find('div.header').should(exist());
+	});
+
+	it('finds a child with a selector function', async () => {
+		await $.open('https://dekolos.github.io/sulfide/tests/todo/todo.html');
+		await $('body').find(byText('Sulfide Example To Do List')).should(exist());
+	});
+
+	it('finds a child by chaining selectors', async () => {
+		await $.open('https://dekolos.github.io/sulfide/tests/todo/todo.html');
+		await $('body').find('div.header').find('h2').should(exist());
+	});
+
+	it('finds a child by chaining selector function', async () => {
+		await $.open('https://dekolos.github.io/sulfide/tests/todo/todo.html');
+		await $('body').find('div.header').find(byText('Sulfide Example To Do List')).should(exist());
+	});
+
+	it('does not find a child of a non-existing parent with a css selector', async () => {
+		await $.open('https://dekolos.github.io/sulfide/tests/todo/todo.html');
+		await $('bodie').find('div.header').shouldNot(exist());
+	});
+
+	it('does not find a child of a non-existing parent with a selector function', async () => {
+		await $.open('https://dekolos.github.io/sulfide/tests/todo/todo.html');
+		await $('bodie').find(byText('Sulfide Example To Do List')).shouldNot(exist());
+	});
+
+	it('does not find a non-existing child of a parent with a css selector', async () => {
+		await $.open('https://dekolos.github.io/sulfide/tests/todo/todo.html');
+		await $('body').find('div.headerssss').shouldNot(exist());
+	});
+
+	it('does not find a non-existing child of a parent with a selector function', async () => {
+		await $.open('https://dekolos.github.io/sulfide/tests/todo/todo.html');
+		await $('body').find(byText('Selenide Example To Do List')).shouldNot(exist());
+	});
+});
