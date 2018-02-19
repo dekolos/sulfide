3432e11b2e1a04abf27953515cac1ecde2914cfc
Ronald Toussaint <r.toussaint@kopano.com>
2018-02-19 13:57:56 +0100

    Add support for NWJS apps
    Adding an option to specify running an NWJS app.
    Overrides the Target.page() function of puppeteer when running an NWJS
    app to be able to get the page handle from a target.
    'targetchanged' listener is added to $.open method when runing an NWJS
    app to get the page handle.
    Updated .eslintrc to support spread operator and dangling underscores.
    
    TODO: sulfide should be able to handle multiple pages.

--- tests/specs/helpers/beforeafter.js
+++ tests/specs/helpers/beforeafter.js
@@ -2,5 +2,5 @@
 global.beforeAll(() => {
 	// Increase the timeout of Jasmine to 20 seconds
 	// This way Chromium has more than 5 seconds to start up for the first test
-	jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000; // 20 seconds
+	jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000; // eslint-disable-line no-magic-numbers
 });
